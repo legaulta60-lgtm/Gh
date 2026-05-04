d9426ab5e03041a97e74d4da0a7a9343e9ae5a28
legaulta60-lgtm <legaulta60@gmail.com>
2026-05-03 23:05:21 -0400

    Update gameResults.js

--- gameResults.js
+++ gameResults.js
@@ -291,6 +291,7 @@ await updateSheetValues("Player Stats!A3:I", Object.values(players));
 if (Object.values(goalies).length) {
 await updateSheetValues("Goalie Stats!A3:I", Object.values(goalies));
 }
-}
+
 
 return { handleGameResults };
+};
